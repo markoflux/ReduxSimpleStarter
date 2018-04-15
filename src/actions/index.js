029cfa0b56b64197c8590eff1007fbb7afeeb734
Bradic, Marko <mbradic@deloitte.de>
2018-04-15 22:57:40 +0200

    built weather app from section 5

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,16 @@
-export function selectBook(book) {
-  // selectBook is an ActionCreator, it needs to return an action, an object with a type property
+import axios from 'axios';
+
+const API_KEY = "0ed67cd6c07a7b2043a1a80d66fa04d6";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},de`;
+  const request = axios.get(url);
+
   return {
-    type: 'BOOK_SELECTED',
-    payload: book
-  };
+    type: FETCH_WEATHER,
+    payload: request
+  }
 }
